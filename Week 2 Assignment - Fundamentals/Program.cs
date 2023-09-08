a7f10fa07d97c2ef0092bab7d7b51c7e681448a8
Nemo Cedillo <nemo120800@hotmail.com>
2023-09-07 20:39:11 -0500

    Updated

--- Week 2 Assignment - Fundamentals/Program.cs
+++ Week 2 Assignment - Fundamentals/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Week_2_Assignment___Fundamentals.UIUtility;
 
 namespace Week_2_Assignment___Fundamentals
 {
@@ -10,7 +12,26 @@ namespace Week_2_Assignment___Fundamentals
     {
         static void Main(string[] args)
         {
+            GameInfo gameInfo = new GameInfo();
 
+
+            //Getting the number of items in the Meta Data
+            int numOfGames = gameInfo.MetaData.Count;
+            //Display on screen
+            Prompt("Number of Games:");
+            Answer($"{numOfGames}");
+
+            //Getting all of the games.
+            SortedList<string,int> genreList = new SortedList<string,int>();
+            foreach (Info i in gameInfo.MetaData)
+            {
+                if (genreList.ContainsKey(i.Genre))
+                {
+
+                } 
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Week 2 Assignment - Fundamentals/UIUtility.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_2_Assignment___Fundamentals
+{
+    static public class UIUtility
+    {
+        static void WriteCentered(string prompt)
+        {
+        }
+
+
+        public static void Prompt(string question)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(question);
+            Console.ResetColor();
+        }
+
+        public static void Answer(string answer)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(answer);
+            Console.ResetColor();
+        }
+    }
+}
