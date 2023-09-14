e37042878dd962edbab5eaf53d1b103a0ed62c36
cedillo02 <cedillo02@VDIIAMIC4QM3-15>
2023-09-14 15:41:05 -0500

    Week 2. IO System Assignment added.

---
+++ Wk 2_File IO Online Intro Learning/DataReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk_2_File_IO_Online_Intro_Learning
+{
+    public class DataReader
+    {
+
+    }
+}
---
+++ Wk 2_File IO Online Intro Learning/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Wk_2_File_IO_Online_Intro_Learning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
