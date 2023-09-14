7b5ad3d2215440592d1f1747356bb109e0634a8c
cedillo02 <cedillo02@VDIIAMIC4QM3-15>
2023-09-14 17:55:22 -0500

    Project currently working. Able to read and print out results from text files.

--- Wk 2_File IO Online Intro Learning/DataReader.cs
+++ Wk 2_File IO Online Intro Learning/DataReader.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,60 @@ namespace Wk_2_File_IO_Online_Intro_Learning
 {
     public class DataReader
     {
+
+        public void InitiateFileRead()
+        {
+            //Directory of files
+            string dir = "../../data";
+            //Get all file names for reading
+            string[] fileNames = Directory.GetFiles(dir);
+
+            //Read Each file
+            foreach (string name in fileNames)
+            {//Set file data to strings
+                string file = new FileInfo(name).Name;
+                string text = File.ReadAllText(name).ToLower();
+
+                //Create a new dictionary
+                Dictionary<char, int> d = CreateDictionary();
+
+                //Read through each char of the text.
+                //Add them to the value of the matching key.
+                foreach (char c in text)
+                    if (d.ContainsKey(c)) d[c] += 1;
+
+                //Print Data
+                PrintData(file, d);
+            }
+        }
+
+
+        //Print the strings
+        void PrintData(string name, Dictionary<char, int> dictionary)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("File: " + name);
+
+            Console.ForegroundColor = ConsoleColor.White;
+            foreach (var key in dictionary)
+                Console.WriteLine($"  {key.Key} : {key.Value}");
+
+            Console.WriteLine();
+            Console.ResetColor();
+        }
+
+        Dictionary<char,int> CreateDictionary()
+        {
+            //Alphabet
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            Dictionary<char, int> dictionary = new Dictionary<char, int>();
+
+            //Create a new dictionary
+            foreach(char c in alphabet)
+                dictionary.Add(c, 0);
+        
+            return dictionary;
+        }
 
     }
 }
--- Wk 2_File IO Online Intro Learning/Program.cs
+++ Wk 2_File IO Online Intro Learning/Program.cs
@@ -9,9 +9,16 @@ namespace Wk_2_File_IO_Online_Intro_Learning
 {
     internal class Program
     {
+        //Credits
+        //Text used in text files were
+        //exerpts from the Prog 260-01 C# Fundementals Guide.
         static void Main(string[] args)
         {
+            //DataReader used to read in data.
+            DataReader data = new DataReader();
+            data.InitiateFileRead();
 
+            Console.ReadLine();
         }
     }
 }
