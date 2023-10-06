762c35643ba27f74c80f76fe5e83faa08562806d
Glareyo <47433630+Glareyo@users.noreply.github.com>
2023-10-06 13:25:03 -0500

    JSON Begun

---
+++ Week 5 - JSON_XML/Wk5_Assignment/JSON Items/Address.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk5_Assignment.JSON_Items
+{
+    // Credit:
+    // Leo Hazou, for providing documentation and presentation of XML file reading,
+    // Code below is an exact copy of the code provided in the presentations with a few minor changes.
+    public class Address
+    {
+        [JsonProperty("streetAddress")]
+        string streetAddress { get; set; }
+        [JsonProperty("city")]
+        string city { get; set; }
+        [JsonProperty("state")]
+        string state { get; set; }
+        [JsonProperty("postalCode")]
+        string postalCode { get; set; }
+    }
+}
---
+++ Week 5 - JSON_XML/Wk5_Assignment/JSON Items/Student.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk5_Assignment.JSON_Items
+{
+    // Credit:
+    // Leo Hazou, for providing documentation and presentation of XML file reading,
+    // Code below is an exact copy of the code provided in the presentations with a few minor changes.
+    public class Student
+    {
+        [JsonProperty("firstName")]
+        string FirstName { get; set; }
+        [JsonProperty("lastName")]
+        string LastName { get; set; }
+        [JsonProperty("isEnrolled")]
+        bool isEnrolled { get; set; }
+        [JsonProperty("YearsEnrolled")]
+        int YearsEnrolled { get; set; }
+        [JsonProperty("address1")]
+        Address Address1 { get; set; }
+        [JsonProperty("address2")]
+        Address Address2 { get; set; }
+        [JsonProperty("phoneNumbers")]
+        List<Phone> PhoneNumbers = new List<Phone>();
+    }
+}
